6118d4383025b4299266a1056b73c0a751865101
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-02-22 21:27:04 -0500

    Update part_2_template_solution.py

--- part_2_template_solution.py
+++ part_2_template_solution.py
@@ -88,11 +88,11 @@ class Section2:
         # return values:
         # Xtrain, ytrain, Xtest, ytest: the data used to fill the `answer`` dictionary
 
-        Xtrain = Xtest = np.zeros([1, 1], dtype="float")
-        ytrain = ytest = np.zeros([1], dtype="int")
+        
 
         return answer, Xtrain, ytrain, Xtest, ytest
-
+    Xtrain = Xtest = np.zeros([1, 1], dtype="float")
+    ytrain = ytest = np.zeros([1], dtype="int")
     """
     B.  Repeat part 1.C, 1.D, and 1.F, for the multiclass problem. 
         Use the Logistic Regression for part F with 300 iterations. 
